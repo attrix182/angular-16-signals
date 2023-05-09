63d712c1c995d6d9acdfa0a9bc796586a8be2d0e
Luciano S <luciano.sinisterra@lamercantil.com.ar>
2023-05-08 22:57:50 -0300

    Feat test signals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { SignalServiceComponent } from './signal-service/signal-service.component';
 import { SignalComponentComponent } from './signal-component/signal-component.component';
+
 
 @NgModule({
   imports: [BrowserModule, ReactiveFormsModule],
-  declarations: [AppComponent, SignalServiceComponent, SignalComponentComponent],
+  declarations: [AppComponent, SignalComponentComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
